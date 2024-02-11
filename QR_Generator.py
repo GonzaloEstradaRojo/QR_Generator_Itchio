d36fe03ac698b4dc710839c9ca2a734db093ece5
GonzaloEstradaRojo <gonero98@gmail.com>
2024-02-11 23:59:14 +0100

    Added try and except and list change to tuples

--- QR_Generator.py
+++ QR_Generator.py
@@ -10,19 +10,22 @@ URL = "https://itch.io/jam/malagajam-weekend-17/entries"
 def Get_Itchio_Data(url):
     # Start Firefox session
     driver = webdriver.Firefox()
-    driver.maximize_window()
-    # Open web application
-    driver.get(URL)
-    # driver.execute_script("window.scrollTo(0, 540)")
-    driver.execute_script("window.scrollTo(0, window.scrollMaxY)")
-    driver.implicitly_wait(30)
-    # Run through all elements and return individual text
-    elems = driver.find_elements(By.CSS_SELECTOR,".label [href]")
-    data = {elem.get_attribute('innerHTML'):elem.get_attribute('href') for elem in elems}
-    # Close browser window
-    driver.quit()
+    try:
+        # Open web application
+        driver.get(URL)
+        # driver.execute_script("window.scrollTo(0, 540)")
+        driver.execute_script("window.scrollTo(0, window.scrollMaxY)")
+        driver.implicitly_wait(30)
+        # Run through all elements and return individual text
+        elems = driver.find_elements(By.CSS_SELECTOR,".label [href]")
+        data = [(elem.get_attribute('innerHTML'),elem.get_attribute('href')) for elem in elems]
+        # Close browser window
+        driver.quit()
+        return data
+    except:
+        print("error")
+        driver.quit()
 
-    return data
 
 if __name__ == "__main__":
     games_data = Get_Itchio_Data(URL)
