4717693d7e6d08b81c1970214041ed3843ff0f35
GonzaloEstradaRojo <gonero98@gmail.com>
2024-02-12 01:44:20 +0100

    Added QR Generation of game links

--- QR_Generator.py
+++ QR_Generator.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import re
+import qrcode
+from PIL import Image 
 
 ############# VARIABLES #############
-
 URL = "https://itch.io/jam/malagajam-weekend-17/entries"
-
 #####################################
 
 def Get_Itchio_Data(url):
@@ -22,10 +23,33 @@ def Get_Itchio_Data(url):
         # Close browser window
         driver.quit()
         return data
-    except:
-        print("error")
-        driver.quit()
+    except Exception as error:
+      print("An error occurred:", error)
+      driver.quit()
 
+
+def Refactor_Game_Name(nombre):
+    # Regex expression for ilegal windows character in files
+    expression = r'[<>:"/\\|?*\x00-\x1F]'
+    return re.sub(expression, ' ', nombre)
 
 if __name__ == "__main__":
-    games_data = Get_Itchio_Data(URL)
+
+    try:
+        games_data = Get_Itchio_Data(URL)
+        a = games_data[0][1]
+        qrs = []
+        for i in range(len(games_data)):
+            print(games_data[i])
+            refactor_Name = Refactor_Game_Name(games_data[i][0])
+            img = qrcode.make(games_data[i][1])
+            # img.save(f'Qrs\{refactor_Name}.png')
+            qrs.append((refactor_Name,img))
+        # img.show()
+        print(a)
+        print(img)
+        print(qrs)
+
+    except Exception as error:
+      print("MAIN")
+      print("An error occurred:", error) 
