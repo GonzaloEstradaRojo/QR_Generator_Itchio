d9f7f27deff76360fbae2eb76c2f6a2620edfda7
GonzaloEstradaRojo <gonero98@gmail.com>
2024-02-16 23:27:37 +0100

    Modified creation of QR. Added logic to create PDF with Table and add names and qrs

--- QR_Generator.py
+++ QR_Generator.py
@@ -1,12 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import re
+import os
 import qrcode
-from PIL import Image 
+import time
+
+#creation of new pdf
+from reportlab.platypus import SimpleDocTemplate, Image, Table, TableStyle
+from reportlab.lib.units import inch
 
 ############# VARIABLES #############
 URL = "https://itch.io/jam/malagajam-weekend-17/entries"
+PDFNAME = "Games Qrs"
+QRSIZE = 180
 #####################################
+
 
 def Get_Itchio_Data(url):
     # Start Firefox session
@@ -15,8 +23,10 @@ def Get_Itchio_Data(url):
         # Open web application
         driver.get(URL)
         # driver.execute_script("window.scrollTo(0, 540)")
-        driver.execute_script("window.scrollTo(0, window.scrollMaxY)")
         driver.implicitly_wait(30)
+        time.sleep(3)
+        driver.execute_script("window.scrollTo(0, window.scrollMaxY)")
+        time.sleep(3)
         # Run through all elements and return individual text
         elems = driver.find_elements(By.CSS_SELECTOR,".label [href]")
         data = [(elem.get_attribute('innerHTML'),elem.get_attribute('href')) for elem in elems]
@@ -31,25 +41,57 @@ def Get_Itchio_Data(url):
 def Refactor_Game_Name(nombre):
     # Regex expression for ilegal windows character in files
     expression = r'[<>:"/\\|?*\x00-\x1F]'
-    return re.sub(expression, ' ', nombre)
+    return re.sub(expression, ' ', nombre).replace("&amp;","&")
 
-if __name__ == "__main__":
+def Create_QR_Images(data):
+    qrs = []
+    if not os.path.exists("Qrs"):
+        os.mkdir("Qrs") 
 
+    for i in range(len(data)):
+        # img = qrcode.make(data[i][1])
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=2,
+        )
+        qr.add_data(data[i][1])
+        qr.make(fit=True)
+        img = qr.make_image(fill_color="black", back_color="white")
+        refactor_Name = Refactor_Game_Name(data[i][0])
+        img.save(f'Qrs\{refactor_Name}.png')
+        qrs.append((refactor_Name,img))
+    
+    return qrs
+
+def Create_PDF_With_Table(data):
+
+    doc = SimpleDocTemplate(f"{PDFNAME} - TABLA 11.pdf",
+                        rightMargin=1,leftMargin=1,
+                        topMargin=1,bottomMargin=1)
+    elems = []
+    rows = []
+    tabStyle = [('ALIGN',(0,0),(-1,-1),'CENTER')] 
+    for index, game in enumerate(data):
+        print(index, game[0])
+        image = Image(f"Qrs/{game[0]}.png",QRSIZE,QRSIZE)
+        if index % 2 == 0:
+            rows.append([game[0]])
+            rows.append([image])
+        else:
+            rows[-2].append(game[0])
+            rows[-1].append(image)
+    table = Table(rows, colWidths=inch*3)
+    table.setStyle(TableStyle(tabStyle))
+    elems.append(table)
+    doc.build(elems)
+
+if __name__ == "__main__":    
     try:
         games_data = Get_Itchio_Data(URL)
-        a = games_data[0][1]
-        qrs = []
-        for i in range(len(games_data)):
-            print(games_data[i])
-            refactor_Name = Refactor_Game_Name(games_data[i][0])
-            img = qrcode.make(games_data[i][1])
-            # img.save(f'Qrs\{refactor_Name}.png')
-            qrs.append((refactor_Name,img))
-        # img.show()
-        print(a)
-        print(img)
-        print(qrs)
+        qrs = Create_QR_Images(games_data)
+        Create_PDF_With_Table(qrs)
 
     except Exception as error:
-      print("MAIN")
       print("An error occurred:", error) 
