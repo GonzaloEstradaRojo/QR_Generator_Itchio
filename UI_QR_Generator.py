c5de47b57f740f8c5bbbe84f8b255f4efb5a9d1e
GonzaloEstradaRojo <gonero98@gmail.com>
2024-06-26 20:43:17 +0200

    Added Thread error handling and progress bar elements added as class properties

--- UI_QR_Generator.py
+++ UI_QR_Generator.py
@@ -15,6 +15,12 @@ class QRGeneratorApp(tk.Tk):
 
         self.prev_val = 0
         self.prev_msg = ""
+        self.exception_list = []
+        self.generator = None
+
+        self.newWindow = None
+        self.progressBar = None
+        self.msg_Label = None
 
         self.ent_SaveFolder = None
         self.ent_logo = None
@@ -77,12 +83,11 @@ class QRGeneratorApp(tk.Tk):
         self.cb_Img.place(x=50, y=170)
 
     def Add_Create_Button(self):
-        self.btn_Create = ttk.Button(text="Create PDF", command = self.Create_PDF)
+        self.btn_Create = ttk.Button(text="Create PDF", command = self.Create_QR_PDF)
         self.btn_Create.place(x=200, y = 250)
 
     def Select_Save_Directory(self):
         self.savedDirectory = filedialog.askdirectory()
-        # print("AAA   " + self.SAVEDIRECTORY)
         if(self.savedDirectory != ""):
             self.ent_SaveFolder.delete(0,tk.END)
             self.ent_SaveFolder.insert(0,self.savedDirectory)
@@ -93,28 +98,41 @@ class QRGeneratorApp(tk.Tk):
             self.ent_logo.delete(0,tk.END)
             self.ent_logo.insert(0,filename)
     
-    def Create_PDF(self):
-        self.browser = self.dropdown.get()
-        generator = QRGenerator()
-        if(self.ent_url.get() == ""):
-            messagebox.showwarning(message=f"URL is missing. \nPlease, fill the URL before creating th PDF",title="Populate URL")
-            return
-        
-        generator.Set_Url(self.ent_url.get())
-        if(self.logoWanted.get() == 1):
-            if(self.ent_logo.get() == ""):
-                messagebox.showwarning(message = f"Logo is missing.\nPlease, select an image before creating th PDF",title = "Populate URL")   
-                return 
-            generator.Set_Add_Logo(True, self.ent_logo.get() )
-        
-        generator.Set_Delete_QR_Folder(self.deleteQrs.get() == 1)
-        generator.Set_Webdriver(self.browser)
-        generator.Set_Save_Directory(self.savedDirectory)
-        self.Enable_Buttons(False) 
-        progressWindow, progressBar, msgLabel = self.Open_Progress_Indicator_Window()
-        main_thread = threading.Thread(target=generator.Create_PDF)
-        main_thread.start()    
-        self.Schedule_Check(main_thread, progressWindow, progressBar, msgLabel, generator)
+    def Thread_Target(self):
+        try:
+            if(self.ent_url.get() == ""):
+                self.exception_list.append("URL is missing. \nPlease, fill the URL before creating th PDF")
+                return
+            
+            self.generator.Set_Url(self.ent_url.get())
+            if(self.logoWanted.get() == 1):
+                logoDirectory = self.ent_logo.get()
+                if(logoDirectory == ""):
+                    self.exception_list.append("Logo is missing.\nPlease, select an image before creating th PDF")   
+                    return 
+                self.generator.Set_Add_Logo(True, logoDirectory)
+            self.generator.Set_Delete_QR_Folder(self.deleteQrs.get() == 1)
+            self.generator.Set_Webdriver(self.browser)
+            self.generator.Set_Save_Directory(self.savedDirectory)
+            self.Enable_Buttons(False) 
+            self.generator.Create_PDF()
+        except Exception as error:
+            self.exception_list.append(error)
+
+
+    def Create_QR_PDF(self):
+        try:
+            self.generator = QRGenerator()
+            self.browser = self.dropdown.get()
+            self.Open_Progress_Indicator_Window()
+            main_thread = threading.Thread(target = self.Thread_Target)
+            main_thread.start()   
+            self.Schedule_Check(main_thread)
+        except Exception as error:
+            self.Enable_Buttons(True)
+            self.Empty_Fields()
+            messagebox.showwarning(message = f"Something went wrong. Try again \n{error}", title = "Error")
+
 
     def Enable_Buttons(self, enabled):
         fields = [self.ent_logo, self.ent_SaveFolder, self.ent_url,  self.dropdown,  self.btn_Create,  self.btn_logo, self.btn_SaveFolder, self.cb_Img, self.cb_QRs]
@@ -122,49 +140,50 @@ class QRGeneratorApp(tk.Tk):
             field["state"] = "normal" if enabled else "disabled"  
     
     def Open_Progress_Indicator_Window(self):
-        newWindow = tk.Toplevel() 
-        newWindow.title("Creating PDF")
-        newWindow.geometry("400x125")
+        self.newWindow = tk.Toplevel() 
+        self.newWindow.title("Creating PDF")
+        self.newWindow.geometry("400x125")
 
-        info_label = ttk.Label(master=newWindow, text="Please, wait a moment until the process is done.")
+        info_label = ttk.Label(master = self.newWindow, text="Please, wait a moment until the process is done.")
         info_label.place(x = 20, y = 20)
 
-        msg_label = ttk.Label(master=newWindow)
-        msg_label.place(x = 20, y = 50)
+        self.msg_Label = ttk.Label(master = self.newWindow)
+        self.msg_Label.place(x = 20, y = 50)
 
-        progressbar = ttk.Progressbar(master=newWindow, mode='determinate')
-        progressbar.place(x=20, y=75, width=350)
+        self.progressBar = ttk.Progressbar(master = self.newWindow, mode='determinate')
+        self.progressBar.place(x=20, y=75, width=350)
 
-        newWindow.focus()
-        return newWindow, progressbar, msg_label
+        self.newWindow.focus()
     
-    def Schedule_Check(self, thread, progressWindow, progressBar, msgLabel, generator):
-
-        new_val = generator.Get_Progress_Number()
-        new_msg = generator.Get_Progress_Message()
+    def Schedule_Check(self, thread):
+        new_val = self.generator.Get_Progress_Number()
+        new_msg = self.generator.Get_Progress_Message()
         if(self.prev_val != new_val ):
-            progressBar.step( new_val - self.prev_val)
+            self.progressBar.step(new_val - self.prev_val)
             self.prev_val = new_val
         if(self.prev_msg != new_msg):
-            msgLabel.config(text = new_msg)
+            self.msg_Label.config(text = new_msg)
             self.prev_msg = new_msg
-        self.after(1000, self.Check_If_Thread_Is_Completed, thread, progressWindow, progressBar, msgLabel, generator)
+        self.after(1000, self.Check_If_Thread_Is_Completed, thread)
+
+            
+
     
-    def Check_If_Thread_Is_Completed(self, thread, progressWindow, progressBar, msgLabel, generator):
+    def Check_If_Thread_Is_Completed(self, thread):
         # If the thread has finished, re-enable the button and show a message.
         if not thread.is_alive():
-            progressWindow.destroy()
-            if(self.prev_val>50):
-                messagebox.showinfo(
-                    message="Process finished. The PDF has been created in the selected folder",
-                    title="PDF Created"
-                )   
+            self.newWindow.destroy()
+            if self.exception_list:
+                error_message = self.exception_list[0]
+                messagebox.showwarning(message=f"Something went wrong.\n{error_message}", title="Error")
+            elif(self.prev_val>50):
+                messagebox.showinfo(message="Process finished. The PDF has been created in the selected folder", title="PDF Created")   
             self.Enable_Buttons(True)
-            self.Empty_Fields()
+            self.Empty_Fields()                
         else:
             # Otherwise check again after one second.
-            self.Schedule_Check(thread, progressWindow, progressBar, msgLabel, generator)
-   
+            self.Schedule_Check(thread)
+        
     def Empty_Fields(self):
         self.ent_SaveFolder.delete(0,tk.END)
         self.ent_url.delete(0,tk.END)
@@ -184,16 +203,8 @@ class QRGeneratorApp(tk.Tk):
             self.ent_logo.place_forget()
             self.ent_logo.delete(0,tk.END)
 
-# class ProgressBarWindow:
-#     def __init__(self):
-#         self.window = tk.Toplevel() 
-#         self.window.title("Creating PDF")
-#         self.window.geometry("400x125")
-
 if __name__ == "__main__":    
     try:
         QRGeneratorApp()
-
     except Exception as error:
-        print("An error occurred: ", error) 
-        messagebox.showwarning(message = f"Something went wrong \n {error}", title = "Error")
+        messagebox.showwarning(message = f"Something went wrong \n{error}", title = "Error")
